5d769cb931354bac0a37bb1c0b7b7f3bdb761b37
Carlo <carlo.nicolo@hotmail.com>
2023-01-04 20:12:10 +0100

    Ownership example with anonymous functions

--- channels/ownership.go
+++ channels/ownership.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// create channel owner goroutine which return channel and
+	// writes data into channel and
+	// closes the channel when done.
+
+	owner := func() chan int {
+		ch := make(chan int)
+		go func() {
+			defer close(ch)
+			for i := 0; i < 6; i++ {
+				ch <- i
+			}
+		}()
+		return ch
+	}
+
+	consumer := func(ch <-chan int) {
+		// read values from channel
+		for v := range ch {
+			fmt.Printf("Received: %d\n", v)
+		}
+		fmt.Println("Done receiving!")
+	}
+
+	// create owner channel
+	ch := owner()
+
+	// create consumer channel
+	consumer(ch)
 }
